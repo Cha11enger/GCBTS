84d4929b149bfeebb5a7105adf006aae37c4efe3
ranjith-edx <r.kumar@edexa.team>
2024-03-29 12:57:47 +0530

    index update

---
+++ .history/index_20240329125543.ts
@@ -0,0 +1,24 @@
+// src/index.ts
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './src/config/database';
+import authRoutes from './src/routes/authRoutes';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
