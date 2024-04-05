8c7be76cf38fe666b65cf6c00b46ca99eb68b5c4
ranjith-edx <r.kumar@edexa.team>
2024-04-06 02:04:55 +0530

    routes changes with post

--- .history/src/routes/index_20240406020401.ts
+++ .history/src/routes/index_20240406020401.ts
@@ -4,7 +4,8 @@ import {redirectToGitHubAuth, handleGitHubCallback, exchangeCodeForToken } from 
 const router = express.Router();
 
 router.get('/oauth/authorize', redirectToGitHubAuth);
-router.post('/oauth/authorize', redirectToGitHubAuth);
+router.get('/oauth/authorize', redirectToGitHubAuth);
+
 router.get('/oauth/authorize/callback', handleGitHubCallback);
 router.post('/oauth/exchange', exchangeCodeForToken);
 
